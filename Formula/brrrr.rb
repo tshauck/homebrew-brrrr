1d678027215c44322b0349d2a8cb7e16e69024d7
Trent Hauck <trent@trenthauck.com>
2020-11-03 21:52:04 -0800

    Update formula.

--- Formula/brrrr.rb
+++ Formula/brrrr.rb
@@ -1,9 +1,9 @@
 class Brrrr < Formula
   desc "CLI goes brrrr..."
   homepage "https://github.com/tshauck/brrrr"
-  url "https://github.com/tshauck/brrrr/releases/download/v0.7.3/brrrr-v0.7.3-x86_64-apple-darwin.tar.gz"
-  sha256 "a350afb266c4c2a1f0cd78d60d05a1fc38a641acb7850ed579b51522b0dc67ed"
-  version "0.7.3"
+  url "https://github.com/tshauck/brrrr/releases/download/v0.7.4/brrrr-v0.7.4-x86_64-apple-darwin.tar.gz"
+  sha256 "e86bcc52a42074283898238a7d6bb391b37b0b3065d1f79d52b0a96d9396835c"
+  version "0.7.4"
 
   def install
     bin.install "brrrr"
